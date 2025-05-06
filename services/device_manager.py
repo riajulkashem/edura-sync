204cf10702615590e59d56a3f5390142638a839a
riajulkashem <riajul.programmer@gmail.com>
2025-05-07 01:41:06 +0600

    added log

--- services/device_manager.py
+++ services/device_manager.py
@@ -168,7 +168,7 @@ class DeviceManager:
                 conn.disable_device()
                 print(f'device connected: {device.ip_address}')
                 users = self.user_repo.filter(saved_to_device=False, device=device.id)
-                print(f'users without device filter {self.user_repo.filter(saved_to_device=False)}')
+                print(f'users without device filter {self.user_repo.model.filter(saved_to_device=False)}')
                 print(f'users to migrate: {len(users)}')
                 migrated = 0
                 for user in users:
